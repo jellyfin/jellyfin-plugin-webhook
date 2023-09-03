01685151aa7aab9c31f742efbdd221d17b0da375
naughtyGitCat <psyduck007@outlook.com>
2023-09-03 15:59:57 +0800

    support for bark

---
+++ Jellyfin.Plugin.Webhook/Destinations/Bark/BarkClient.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Jellyfin.Plugin.Webhook.Extensions;
+using MediaBrowser.Common.Net;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+
+namespace Jellyfin.Plugin.Webhook.Destinations.Bark;
+
+/// <summary>
+/// Client for the <see cref="BarkOption"/>.
+/// </summary>
+public class BarkClient : BaseClient, IWebhookClient<BarkOption>
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<BarkClient> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BarkClient"/> class.
+    /// </summary>
+    /// <param name="httpClientFactory">Instance of the <see cref="IHttpClientFactory"/>.</param>
+    /// <param name="logger">Instance of the <see cref="ILogger{BarkClient}"/> interface.</param>
+    public BarkClient(
+        IHttpClientFactory httpClientFactory,
+        ILogger<BarkClient> logger)
+    {
+        _httpClientFactory = httpClientFactory;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// MergeOptionToData
+    /// merge option properties to data dict.
+    /// </summary>
+    /// <param name="option">the bark option.</param>
+    /// <param name="data">the event data.</param>
+    /// <returns>merged data.</returns>
+    public Dictionary<string, object> MergeOptionToData(BarkOption option, Dictionary<string, object> data)
+    {
+        option ??= new BarkOption();
+        var wrappedData = new Dictionary<string, object>
+        {
+            ["data"] = data
+        };
+        _logger.LogWarning("data is null");
+        if (!string.IsNullOrEmpty(option.DeviceKey))
+        {
+            wrappedData["device_key"] = option.DeviceKey;
+        }
+        else
+        {
+            _logger.LogWarning("device_key is empty");
+        }
+
+        if (!string.IsNullOrWhiteSpace(option.Level))
+        {
+            wrappedData["level"] = option.Level;
+        }
+
+        if (!string.IsNullOrEmpty(option.Icon))
+        {
+            wrappedData["icon"] = option.Icon;
+        }
+
+        if (!string.IsNullOrEmpty(option.Group))
+        {
+            wrappedData["isArchive"] = option.Group;
+        }
+
+        if (!string.IsNullOrEmpty(option.JumpUrl))
+        {
+            wrappedData["url"] = option.JumpUrl;
+        }
+
+        return wrappedData;
+    }
+
+    /// <inheritdoc />
+    public async Task SendAsync(BarkOption option, Dictionary<string, object> data)
+    {
+        try
+        {
+            if (!SendWebhook(_logger, option, data))
+            {
+                return;
+            }
+
+            data = MergeOptionToData(option, data);
+            var body = option.GetMessageBody(data);
+            _logger.LogDebug("SendAsync Body: {@Body}", body);
+            using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, option.WebhookUri);
+            foreach (var header in option.Headers)
+            {
+                if (string.IsNullOrEmpty(header.Key) || string.IsNullOrEmpty(header.Value))
+                {
+                    continue;
+                }
+
+                // should not change content type via manually set headers
+                if (string.Equals(HeaderNames.ContentType, header.Key, StringComparison.OrdinalIgnoreCase)
+                    && !string.IsNullOrEmpty(header.Value))
+                {
+                    _logger.LogDebug("should not change content type via manually set headers");
+                }
+                else
+                {
+                    httpRequestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value);
+                }
+            }
+
+            httpRequestMessage.Content = new StringContent(body, Encoding.UTF8, "application/json");
+            using var response = await _httpClientFactory
+                .CreateClient(NamedClient.Default)
+                .SendAsync(httpRequestMessage)
+                .ConfigureAwait(false);
+            await response.LogIfFailedAsync(_logger).ConfigureAwait(false);
+        }
+        catch (HttpRequestException e)
+        {
+            _logger.LogWarning(e, "Error sending notification");
+        }
+    }
+}
---
+++ Jellyfin.Plugin.Webhook/Destinations/Bark/BarkOption.cs
@@ -0,0 +1,64 @@
+﻿namespace Jellyfin.Plugin.Webhook.Destinations.Bark;
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Jellyfin.Extensions.Json;
+using Jellyfin.Plugin.Webhook.Destinations.Generic;
+
+/// <summary>
+/// Bark specific options.
+/// </summary>
+public class BarkOption : BaseOption
+{
+    /// <summary>
+    /// Gets or sets the Device key.
+    /// </summary>
+    [JsonPropertyName("device_key")]
+    public string? DeviceKey { get; set; }
+
+    /// <summary>
+    /// Gets or sets 推送中断级别
+    /// active：默认值，系统会立即亮屏显示通知.
+    /// timeSensitive：时效性通知，可在专注状态下显示通知.
+    /// passive：仅将通知添加到通知列表，不会亮屏提醒.
+    /// </summary>
+    [JsonPropertyName("level")]
+    public string? Level { get; set; }
+
+    /// <summary>
+    /// Gets or sets icon url
+    /// 为推送设置自定义图标，设置的图标将替换默认Bark图标。
+    /// 图标会自动缓存在本机，相同的图标 URL 仅下载一次.
+    /// </summary>
+    [JsonPropertyName("icon")]
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// Gets or sets Group
+    /// 对消息进行分组，推送将按group分组显示在通知中心中。也可在历史消息列表中选择查看不同的群组.
+    /// </summary>
+    [JsonPropertyName("group")]
+    public string? Group { get; set; }
+
+    /// <summary>
+    /// Gets or sets Group IsArchive
+    /// 传 1 保存推送，传其他的不保存推送，不传按APP内设置来决定是否保存.
+    /// </summary>
+    [JsonPropertyName("isArchive")]
+    public int? IsArchive { get; set; }
+
+    /// <summary>
+    /// Gets or sets JumpUrl
+    /// 点击推送时，跳转的URL ，支持URL Scheme 和 Universal Link.
+    /// </summary>
+    [JsonPropertyName("url")]
+    public string? JumpUrl { get; set; }
+
+    /// <summary>
+    /// Gets or sets extra Headers other than application/json.
+    /// </summary>
+    public GenericOptionValue[] Headers { get; set; } = Array.Empty<GenericOptionValue>();
+}
