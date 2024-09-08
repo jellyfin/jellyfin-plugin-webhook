1db059687be68cb199cec843de36f18d10464e7d
chiragkrishna <chiragkrishna@gmail.com>
2024-09-08 11:06:18 +0530

    Undo Changes

--- Jellyfin.Plugin.Webhook/Models/QueuedItemContainer.cs
+++ Jellyfin.Plugin.Webhook/Models/QueuedItemContainer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Jellyfin.Plugin.Webhook.Models;
 
@@ -11,33 +11,19 @@ public class QueuedItemContainer
     /// Initializes a new instance of the <see cref="QueuedItemContainer"/> class.
     /// </summary>
     /// <param name="id">The item id.</param>
-    /// <param name="name">The item name (nullable).</param>
-    /// <param name="itemType">The item type.</param>
-    public QueuedItemContainer(Guid id, string? name = null, string? itemType = null)
+    public QueuedItemContainer(Guid id)
     {
         ItemId = id;
-        Name = name;
-        ItemType = itemType;
         RetryCount = 0;
     }
-
-    /// <summary>
-    /// Gets or sets the item name.
-    /// </summary>
-    public string? Name { get; set; }
-
-    /// <summary>
-    /// Gets or sets the item type.
-    /// </summary>
-    public string? ItemType { get; set; }
-
-    /// <summary>
-    /// Gets or sets the current item id.
-    /// </summary>
-    public Guid ItemId { get; set; }
 
     /// <summary>
     /// Gets or sets the current retry count.
     /// </summary>
     public int RetryCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets the current item id.
+    /// </summary>
+    public Guid ItemId { get; set; }
 }
